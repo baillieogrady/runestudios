66e80a9183127e5f4148bdaeb5562a597c12d2fd
Oats <baillieogrady@Baillies-MacBook-Air.local>
2026-05-26 17:02:22 +0100

    make whole button block clickable

--- template-parts/flexible/button.php
+++ template-parts/flexible/button.php
@@ -18,9 +18,9 @@ if ( is_array( $link ) ) {
 ?>
 
 <?php if ( $link_url ) : ?>
-    <section class="bg-black text-white text-center py-28 rounded-4xl">
+    <section class="bg-black text-white text-center rounded-4xl">
         <h2 class="text-[4rem] tracking-[-2px] leading-[1.2]">
-            <a href="<?php echo esc_url( $link_url ); ?>"<?php echo $link_target ? ' target="' . esc_attr( $link_target ) . '" rel="noopener noreferrer"' : ''; ?>>
+            <a class="py-28 block" href="<?php echo esc_url( $link_url ); ?>"<?php echo $link_target ? ' target="' . esc_attr( $link_target ) . '" rel="noopener noreferrer"' : ''; ?>>
                 <?php echo esc_html( $link_title ); ?>
             </a>
         </h2>
